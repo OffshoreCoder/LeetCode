4411b5fa82785e232613d6ee6928592969ba145f
aleksandrbreusov <alexsbreusov@gmail.com>
2024-06-18 00:25:23 +0300

    feat: solve task 27 " Remove Element"

---
+++ src/main/java/Task27/Solution.java
@@ -0,0 +1,19 @@
+package Task27;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(removeElement(new int[]{5}, 5));
+        System.out.println(removeElement(new int[]{3, 2, 2, 3}, 3));
+        System.out.println(removeElement(new int[]{0, 1, 2, 2, 3, 0, 4, 2}, 2));
+    }
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
