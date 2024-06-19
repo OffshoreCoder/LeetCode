518d39d3aa5d0616167254ceeaaecca7b7309a4e
aleksandrbreusov <alexsbreusov@gmail.com>
2024-06-19 21:39:34 +0300

    refactor: optimize solution for task 205 "Isomorphic Strings"

--- src/main/java/Task205/Solution.java
+++ src/main/java/Task205/Solution.java
@@ -10,21 +10,26 @@ public class Solution {
         System.out.println(new Solution().isIsomorphic("badc", "baba"));
     }
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Character> map = new HashMap<>();
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+
         for (int i = 0; i < s.length(); i++) {
             char sc = s.charAt(i);
             char tc = t.charAt(i);
-            if (map.containsKey(sc)) {
-                if (map.get(sc) != tc) {
-                    return false;
-                }
-            } else {
-                if (map.containsValue(tc)) {
-                    return false;
-                }
-                map.put(sc, tc);
+
+            if (mapS[sc] != mapT[tc]) {
+                return false;
             }
+
+            mapS[sc] = i + 1;
+            mapT[tc] = i + 1;
         }
+
         return true;
     }
+
 }
